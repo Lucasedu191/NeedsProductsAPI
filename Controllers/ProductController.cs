a13996ac93a0829bc18f6638023f197cd5016259
Lucas Eduardo <lucasatletico@hotmail.com>
2021-09-25 01:28:40 -0300

    Setting the project, install de swagger lib

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Needs.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Needs.API.Controllers {
+    //exemplo de como fica a url:
+    //urlbase/product/3/byid/4455/?cordoproduto=verde
+    [Route("[controller]")]
+    public class ProductController : ControllerBase {
+        [HttpGet("{id}")]
+        public ProductBase GetById([FromRoute] string id) {
+            return new ProductBase();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -14,10 +14,17 @@ namespace Needs.API {
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
