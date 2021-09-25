6df5d3a7ea4529edfe81b90b6be48cf625e239ba
Lucas Eduardo <lucasatletico@hotmail.com>
2021-09-25 02:40:30 -0300

    Create Controllers, ORM, Entities

---
+++ ORM/NeedsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Needs.API.ORM {
+    public class NeedsDbContext : DbContext {
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,6 +15,7 @@ namespace Needs.API {
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
+            services.AddDbContext<ORM.NeedsDbContext>(opt => opt.UseInMemoryDatabase("NeedsDB"));
             services.AddSwaggerGen();
         }
 
